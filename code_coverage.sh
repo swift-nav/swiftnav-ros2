ee9b7ef7c24a623698a57906fe7812bad54b9624
Krzysztof Naglik <krzysztofnaglik96@gmail.com>
2022-08-25 13:09:59 +0200

    Fix

--- code_coverage.sh
+++ code_coverage.sh
@@ -11,13 +11,17 @@ export CFLAGS="--coverage"
 cmake ..
 build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -j6 all
 make test
-gcovr -j 8 --gcov-executable gcov --sonarqube ./code_coverage.xml --root .
+cd ..
+gcovr -j 8 --gcov-executable gcov --sonarqube ./build/code_coverage.xml --root . ./build
 
-sonar-scanner -X -Dproject.settings=../.github/workflows/sonar-project.properties \
-                 -Dsonar.cfamily.build-wrapper-output="build_wrapper_output_directory" \
+
+cat ./build/code_coverage.xml
+
+sonar-scanner -X -Dproject.settings=.github/workflows/sonar-project.properties \
+                 -Dsonar.cfamily.build-wrapper-output="./build/build_wrapper_output_directory" \
                  -Dsonar.cfamily.cache.enabled=false \
-                 -Dsonar.cfamily.compile-commands=./compile_commands.json \
-                 -Dsonar.coverageReportPaths=./code_coverage.xml \
+                 -Dsonar.cfamily.compile-commands=./build/compile_commands.json \
+                 -Dsonar.coverageReportPaths=./build/code_coverage.xml \
                  -Dsonar.organization=swift-nav \
                  -Dsonar.projectKey=swift-nav_swiftnav-ros2 \
                  -Dsonar.host.url="https://sonarcloud.io" \
