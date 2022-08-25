0c11135cba491d5b180fd45be7db751a1c3cb8dd
Krzysztof Naglik <krzysztofnaglik96@gmail.com>
2022-08-25 13:21:45 +0200

    Test

--- code_coverage.sh
+++ code_coverage.sh
@@ -9,16 +9,15 @@ mkdir build
 cd build
 export CFLAGS="--coverage"
 cmake ..
-build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -j6 all
-make test
+# build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -j6 all
+make -j8 all
+make -j8 test
 cd ..
 gcovr -j 8 --gcov-executable gcov --sonarqube ./build/code_coverage.xml --root . ./build
-
 
 cat ./build/code_coverage.xml
 
 sonar-scanner -X -Dproject.settings=.github/workflows/sonar-project.properties \
-                 -Dsonar.cfamily.build-wrapper-output="./build/build_wrapper_output_directory" \
                  -Dsonar.cfamily.cache.enabled=false \
                  -Dsonar.cfamily.compile-commands=./build/compile_commands.json \
                  -Dsonar.coverageReportPaths=./build/code_coverage.xml \
@@ -27,3 +26,4 @@ sonar-scanner -X -Dproject.settings=.github/workflows/sonar-project.properties \
                  -Dsonar.host.url="https://sonarcloud.io" \
                  -Dsonar.pullrequest.branch=sokhealy/sonarcloud \
                  -Dsonar.pullrequest.key=8
+                #  -Dsonar.cfamily.build-wrapper-output="./build/build_wrapper_output_directory" \
