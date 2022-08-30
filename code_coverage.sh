7fd9cabb7e2997e74d5085d680c24f725429c629
Krzysztof Naglik <krzysztofnaglik96@gmail.com>
2022-08-30 14:42:14 +0200

    Test

--- code_coverage.sh
+++ code_coverage.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
 # arguments:
-# 1 - number of jobs to use in parallel
-# 2 - pull request branch name
-# 3 - pull request number
+# 1 - github token
+# 2 - sonar token
+# 3 - number of jobs to use in parallel
+# 4 - pull request branch name
+# 5 - pull request number
 
 # set -e
+
+export GITHUB_TOKEN=$1
+export SONAR_TOKEN=$2
 
 mkdir -p build
 cd build
 cmake -DCMAKE_C_FLAGS=--coverage -DCMAKE_CXX_FLAGS=--coverage ..
 
-make -j$1 all
-make -j$1 test
+make -j$3 all
+make -j$3 test
 
 cd ..
 gcovr -j $1 --gcov-executable gcov --sonarqube ./build/code_coverage.xml --root . ./build
 
 if [ -n "$2" ] && [ -n "$3" ]; then
     sonar-scanner -X -Dproject.settings=.github/workflows/sonar-project.properties \
-                 -Dsonar.cfamily.cache.enabled=false \
-                 -Dsonar.cfamily.compile-commands=./build/compile_commands.json \
-                 -Dsonar.coverageReportPaths=./build/code_coverage.xml \
-                 -Dsonar.organization=swift-nav \
-                 -Dsonar.projectKey=swift-nav_swiftnav-ros2 \
-                 -Dsonar.host.url="https://sonarcloud.io" \
-                 -Dsonar.pullrequest.branch=$2 \
-                 -Dsonar.pullrequest.key=$3
+                     -Dsonar.cfamily.cache.enabled=false \
+                     -Dsonar.cfamily.compile-commands=./build/compile_commands.json \
+                     -Dsonar.coverageReportPaths=./build/code_coverage.xml \
+                     -Dsonar.organization=swift-nav \
+                     -Dsonar.projectKey=swift-nav_swiftnav-ros2 \
+                     -Dsonar.host.url="https://sonarcloud.io" \
+                     -Dsonar.pullrequest.branch=$4 \
+                     -Dsonar.pullrequest.key=$5
 else
     sonar-scanner -X -Dproject.settings=.github/workflows/sonar-project.properties \
-                 -Dsonar.cfamily.cache.enabled=false \
-                 -Dsonar.cfamily.compile-commands=./build/compile_commands.json \
-                 -Dsonar.coverageReportPaths=./build/code_coverage.xml \
-                 -Dsonar.organization=swift-nav \
-                 -Dsonar.projectKey=swift-nav_swiftnav-ros2 \
-                 -Dsonar.host.url="https://sonarcloud.io" \
-                 -Dsonar.branch.name=master
+                     -Dsonar.cfamily.cache.enabled=false \
+                     -Dsonar.cfamily.compile-commands=./build/compile_commands.json \
+                     -Dsonar.coverageReportPaths=./build/code_coverage.xml \
+                     -Dsonar.organization=swift-nav \
+                     -Dsonar.projectKey=swift-nav_swiftnav-ros2 \
+                     -Dsonar.host.url="https://sonarcloud.io" \
+                     -Dsonar.branch.name=master
 fi
